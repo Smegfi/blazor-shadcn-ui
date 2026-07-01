92a461669914832fb27988e2be25ae3f519748e1
Filip Šmehyl <filip.smehyl@i.cz>
2026-07-01 16:05:51 +0200

    InputGroupInput - Demo - TwoWay binding and Debounce demos
    Added Two-Way data binding Demo and Debounce demo

--- demo/NeoUI.Demo.Shared/Pages/Components/CodeExamples/InputGroupDemo.cs
+++ demo/NeoUI.Demo.Shared/Pages/Components/CodeExamples/InputGroupDemo.cs
@@ -98,6 +98,38 @@ namespace NeoUI.Demo.Shared.Pages.Components
                 </InputGroup>
                 """;
 
+        private const string _twoWayBindingCode = """
+                <Label>Search</Label>
+                <InputGroup>
+                    <InputGroupAddon Align="InputGroupAlign.InlineStart">
+                        <LucideIcon Name="search" Size="16" />
+                    </InputGroupAddon>
+                    <InputGroupInput Placeholder="Search..." @bind-Value="_inputGroupText" />
+                </InputGroup>
+                
+                <span>Text: @_inputGroupText</span>
+                
+                @code {
+                    private string _inputGroupText = "";
+                }
+                """;
+
+        private const string _debounceCode = """
+                <Label>Search</Label>
+                <InputGroup>
+                    <InputGroupAddon Align="InputGroupAlign.InlineStart">
+                        <LucideIcon Name="search" Size="16" />
+                    </InputGroupAddon>
+                    <InputGroupInput Placeholder="Search..." @bind-Value="_debouncedInputGroupText" DebounceDelay="500" />
+                </InputGroup>
+
+                <span>Text: @_debouncedInputGroupText</span>
+
+                @code {
+                    private string _debouncedInputGroupText = "";
+                }
+                """;
+
         private const string _dropdownCode = """
                 <DropdownMenu>
                     <InputGroup>
